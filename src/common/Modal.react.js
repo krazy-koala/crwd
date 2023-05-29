23db716c5260fd3859a0d59bc788fb3d4b8a9045
krazy-koala <allen.lin85@gmail.com>
2023-05-28 18:56:38 -0700

    [CRWD] Add focus trap to Modal and allow pressing ESC key to close modal

--- src/common/Modal.react.js
+++ src/common/Modal.react.js
@@ -1,6 +1,9 @@
+import FocusTrap from 'focus-trap-react';
+
 import Button from './Button.react';
 
 import './Modal.react.css';
+import { useEffect } from 'react';
 
 /**
  * Component Interface:
@@ -13,8 +16,6 @@ import './Modal.react.css';
  * }
  */
 
-// TODO: Press ESC key should close modal.
-// TODO: When modal is open, focus should be trapped inside (e.g. tabbing should not go outside of the modal).
 function Modal(props) {
   const {
     isVisible,
@@ -23,40 +24,54 @@ function Modal(props) {
     title,
     children,
   } = props;
+
+  useEffect(() => {
+    function handleEscapeKey(event) {
+      if (event.code === 'Escape') {
+        onCancel()
+      }
+    }
+  
+    document.addEventListener('keydown', handleEscapeKey)
+    return () => document.removeEventListener('keydown', handleEscapeKey)
+  }, [onCancel]);
+
   return isVisible && (
-    <div className="modal-overlay">
-      <div className="modal">
-        <section className="modal-header">
-          <h3 className="modal-title">{title}</h3>
-          <button
-            class="modal-close-button"
-            onClick={onCancel}
-            aria-label="Close"
-          >
-            X
-          </button>
-        </section>
-        <section className="modal-body">{children}</section>
-        <section className="modal-footer">
-          <Button
-            className="modal-action-button"
-            type="secondary"
-            onClick={onCancel}
-            ariaLabel="Cancel"
-          >
-            Cancel
-          </Button>
-          <Button
-            className="modal-action-button"
-            type="primary"
-            onClick={onConfirm}
-            aria-label="Confirm"
-          >
-            Confirm
-          </Button>
-        </section>
+    <FocusTrap>
+      <div className="modal-overlay">
+        <div className="modal">
+          <section className="modal-header">
+            <h3 className="modal-title">{title}</h3>
+            <button
+              class="modal-close-button"
+              onClick={onCancel}
+              aria-label="Close"
+            >
+              X
+            </button>
+          </section>
+          <section className="modal-body">{children}</section>
+          <section className="modal-footer">
+            <Button
+              className="modal-action-button"
+              type="secondary"
+              onClick={onCancel}
+              ariaLabel="Cancel"
+            >
+              Cancel
+            </Button>
+            <Button
+              className="modal-action-button"
+              type="primary"
+              onClick={onConfirm}
+              aria-label="Confirm"
+            >
+              Confirm
+            </Button>
+          </section>
+        </div>
       </div>
-    </div>
+    </FocusTrap>
   );
 }
 
